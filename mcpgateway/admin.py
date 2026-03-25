82f348d6dbabf27d56ef3ea53b4f322eaa570c79
Suresh Kumar Moharajan <suresh.kumar.m@ibm.com>
2026-03-25 14:30:35 +0000

    fix precommit issues
    Signed-off-by: Suresh Kumar Moharajan <suresh.kumar.m@ibm.com>

--- mcpgateway/admin.py
+++ mcpgateway/admin.py
@@ -140,7 +140,7 @@ from mcpgateway.services.openapi_service import fetch_and_extract_schemas
 from mcpgateway.services.performance_service import get_performance_service
 from mcpgateway.services.permission_service import PermissionService
 from mcpgateway.services.plugin_service import get_plugin_service
-from mcpgateway.services.prompt_service import PromptArgumentsJSONError, PromptNameConflictError, PromptNotFoundError, PromptService
+from mcpgateway.services.prompt_service import PromptNameConflictError, PromptNotFoundError, PromptService
 from mcpgateway.services.resource_service import ResourceNotFoundError, ResourceService, ResourceURIConflictError
 from mcpgateway.services.root_service import RootService, RootServiceError, RootServiceNotFoundError
 from mcpgateway.services.server_service import ServerError, ServerLockConflictError, ServerNameConflictError, ServerNotFoundError, ServerService
